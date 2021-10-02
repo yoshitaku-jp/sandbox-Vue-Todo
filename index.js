1ff9ea30c71dd2514758e7ca6e4c44cbb65fa463
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-10-02 15:25:12 +0900

    localstorage対応

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+const STORAGE_KEY = "sandbox-Vue-Todo";
+const todoStorage = {
+  fetch: function () {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todos.forEach(function (todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  },
+};
+
 var app = new Vue({
   el: "#app",
   data: {
     newItem: "",
-    todos: [],
+    todos: todoStorage.fetch(),
+  },
+  watch: {
+    todos: {
+      handler: function (todos) {
+        todoStorage.save(todos);
+      },
+      deep: true,
+    },
   },
   methods: {
     addTodo: function () {
